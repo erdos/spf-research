25e3a1b10a13ed24b0e0af0fe64e6a42b06dee7b
Janos Erdos <janos.erdos@dbx.hu>
2017-12-16 23:19:02 +0100

    done

--- spf/src/spf/constructive.clj
+++ spf/src/spf/constructive.clj
@@ -11,38 +11,41 @@
 (def deg180 Math/PI)
 (def sqrt3-1 (/ (Math/sqrt 3)))
 
-(defn- spidron-arm-pts [x1 y1 x2 y2 left?]
+(defn- spidron-arm-pts [[x1 y1] [x2 y2] left?]
   (let [sc    (* sqrt3-1 (Math/hypot (- x1 x2) (- y1 y2)))
         angle (+ (Math/atan2 (- y1 y2) (- x2 x1)) (/ Math/PI 3) (if left? deg60 0))]
-    (for [i (range 8)
-          :let [rotation (+ angle (* i deg30))
-                scaling  (* sc (Math/pow sqrt3-1 i))]]
-      [(+ x1 (* scaling (Math/sin rotation)))
-       (+ y1 (* scaling (Math/cos rotation)))])))
+    (concat
+     (for [i (range 8)
+           :let [rotation (+ angle (* i deg30))
+                 scaling  (* sc (Math/pow sqrt3-1 i))]
+           :while (or (= 0 i) (> scaling 4)) ;; ezzel optimalizalhatunk!
+           ]
+       [(+ x1 (* scaling (Math/sin rotation)))
+        (+ y1 (* scaling (Math/cos rotation)))])
+     [[x1 y1]])))
 
-(defn spidron-arm-side [x1 y1 x2 y2 left? n]
+(defn spidron-arm-side  [n [x1 y1 :as p1] [x2 y2 :as p2]]
   (if-not (pos? n)
     [[x1 y1] [x2 y2]]
-    (let [pts  (spidron-arm-pts x1 y1 x2 y2 left?)
-          ys   (mapcat (fn [[a b] [c d]] (spidron-arm-side a b c d false (dec n)))
-                       pts (next pts))
+    (let [pts  (spidron-arm-pts p1 p2 false)
+          ys   (mapcat (partial spidron-arm-side (dec n)) pts (next pts))
 
-          pts2 (spidron-arm-pts x2 y2 x1 y1 (not left?))
-          zs   (mapcat (fn [[a b] [c d]] (spidron-arm-side a b c d false (dec n)))
-                       pts2 (next pts2))]
+          pts2 (spidron-arm-pts p2 p1 true)
+          zs   (mapcat (partial spidron-arm-side (dec n)) pts2 (next pts2))]
       (concat (reverse ys) (next zs)))))
 
+(defn spidron-silhouette  [n [x1 y1 :as p1] [x2 y2 :as p2]]
+  (concat (spidron-arm-side STEPS [x1 y1] [x2 y2])
+          (next (spidron-arm-side STEPS [x2 y2] [x1 y1]))))
 
 (defn draw-lines [pts]
   (doseq [[[x1 y1] [x2 y2]] (map vector pts (next pts))] (q/line x1 y1 x2 y2)))
 
-(def STEPS 3)
+(def STEPS 5)
 
 (defn spidron-arm [x1 y1 x2 y2]
   (q/stroke 255 0 0)
-  (draw-lines (spidron-arm-side x1 y1 x2 y2 false STEPS))
-  (q/stroke 0 0 244)
-  (draw-lines (spidron-arm-side x1 y1 x2 y2 true STEPS)))
+  (draw-lines (spidron-silhouette STEPS [x1 y1] [x2 y2])))
 
 (defn update-state [s] s)
 
