d59206f43c352ba5cd6cd5a01308cf1dada0052e
Janos Erdos <janos.erdos@dbx.hu>
2017-12-16 23:03:15 +0100

    wip

--- spf/src/spf/constructive.clj
+++ spf/src/spf/constructive.clj
@@ -11,58 +11,52 @@
 (def deg180 Math/PI)
 (def sqrt3-1 (/ (Math/sqrt 3)))
 
-(defn spidron-arm-half-pts [x y angle sc]
-  (let [angle (+ angle (/ Math/PI 3))]
-    (for [i (range 10)]
-      (let [rotation (+ angle (* i deg30))
-            scaling  (* sc (Math/pow sqrt3-1 i))]
-        [(+ x (* scaling (Math/sin rotation)))
-         (+ y (* scaling (Math/cos rotation)))]))))
+(defn- spidron-arm-pts [x1 y1 x2 y2 left?]
+  (let [sc    (* sqrt3-1 (Math/hypot (- x1 x2) (- y1 y2)))
+        angle (+ (Math/atan2 (- y1 y2) (- x2 x1)) (/ Math/PI 3) (if left? deg60 0))]
+    (for [i (range 8)
+          :let [rotation (+ angle (* i deg30))
+                scaling  (* sc (Math/pow sqrt3-1 i))]]
+      [(+ x1 (* scaling (Math/sin rotation)))
+       (+ y1 (* scaling (Math/cos rotation)))])))
 
-(defn spidron-arm-pts [x1 y1 x2 y2]
-  (let [dist (Math/hypot (- x1 x2) (- y1 y2))
-        r    (/ dist 2 (Math/cos (/ Math/PI 6)))
-        d    (Math/atan2 (- y1 y2) (- x2 x1))
-        pts (spidron-arm-half-pts x1 y1 d r)
-        pts2 (spidron-arm-half-pts x2 y2 (- d deg120) r)]
-    (concat (reverse pts) (next pts2))))
+(defn spidron-arm-side [x1 y1 x2 y2 left? n]
+  (if-not (pos? n)
+    [[x1 y1] [x2 y2]]
+    (let [pts  (spidron-arm-pts x1 y1 x2 y2 left?)
+          ys   (mapcat (fn [[a b] [c d]] (spidron-arm-side a b c d false (dec n)))
+                       pts (next pts))
 
-(defn spidron-arm-pts* [n x1 y1 x2 y2]
-  (if (pos? n)
-    (let [pts (spidron-arm-pts* (dec n) x1 y1 x2 y2)]
-      (for [[[x1 y1] [x2 y2]] (map vector pts (next pts))
-            pt (spidron-arm-pts x1 y1 x2 y2)]
-        pt))
-    [[x1 y1] [x2 y2]]))
+          pts2 (spidron-arm-pts x2 y2 x1 y1 (not left?))
+          zs   (mapcat (fn [[a b] [c d]] (spidron-arm-side a b c d false (dec n)))
+                       pts2 (next pts2))]
+      (concat (reverse ys) (next zs)))))
 
-                                        ; (spidron-arm-pts* 1 0 0 100 100)
 
-(def STEPS 1)
+(defn draw-lines [pts]
+  (doseq [[[x1 y1] [x2 y2]] (map vector pts (next pts))] (q/line x1 y1 x2 y2)))
 
-(defn spidron-arm-side [x1 y1 x2 y2]
-  (let [pts (spidron-arm-pts* STEPS x1 y1 x2 y2)]
-    (println :> pts)
-    (doseq [[[x1 y1] [x2 y2]] (map vector pts (next pts))]
-      (q/line x1 y1 x2 y2))))
+(def STEPS 3)
 
 (defn spidron-arm [x1 y1 x2 y2]
-  (spidron-arm-side x1 y1 x2 y2)
-  (spidron-arm-side x2 y2 x1 y1))
+  (q/stroke 255 0 0)
+  (draw-lines (spidron-arm-side x1 y1 x2 y2 false STEPS))
+  (q/stroke 0 0 244)
+  (draw-lines (spidron-arm-side x1 y1 x2 y2 true STEPS)))
 
 (defn update-state [s] s)
 
 (defn draw-state [state]
   (q/background 233 243 55)
-  ;(println :>>!!)
-  (try (spidron-arm 200 200 544 555) (catch Exception e (println e))))
+  (try (spidron-arm 200 200 544 555) (catch Exception e nil)))
 
 (defstate sketch
-  :start (q/sketch :size [960 960]
-                   :setup #'setup
-                   :update #'update-state
-                   :draw #'draw-state
-                   :features [:keep-on-top]
-                   :middleware [m/fun-mode])
-  :stop (.exit sketch))
+  :start (future (q/sketch :size [960 960]
+                    :setup #'setup
+                    :update #'update-state
+                    :draw #'draw-state
+                    :features [:keep-on-top]
+                    :middleware [m/fun-mode]))
+  :stop (future (.exit @sketch)))
 
 ; (mount.core/start spf.constructive/sketch)
